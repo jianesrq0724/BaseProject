0443d7aeb41092ca60b4cfeace39bde0ac58e972
shenruiqin <shenruiqin@100cb.cn>
2017-07-04 17:44:56 +0800

    提交

---
+++ app/src/main/java/com/ruiqin/baseproject/base/BaseFragment.java
@@ -0,0 +1,55 @@
+package com.ruiqin.baseproject.base;
+
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ruiqin.baseproject.util.InstanceUtil;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+/**
+ * Created by ruiqin.shen
+ * 类说明：
+ */
+
+public abstract class BaseFragment<P extends BasePresenter, M extends BaseModel> extends Fragment {
+    private Unbinder mBind;
+    public P mPresenter;
+    public Context mContext;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        mBind = ButterKnife.bind(this, view);
+        if (this instanceof BaseView) {
+            mPresenter = InstanceUtil.getInstance(this, 0);
+            mPresenter.setVM(this, InstanceUtil.getInstance(this, 1));
+        }
+        initView(view, savedInstanceState);
+        return view;
+    }
+
+    protected abstract void initView(View view, Bundle savedInstanceState);
+
+    protected abstract int getLayoutId();
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBind.unbind();
+    }
+}
---
+++ app/src/main/java/com/ruiqin/baseproject/module/BlankFragment.java
@@ -0,0 +1,19 @@
+package com.ruiqin.baseproject.module;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.ruiqin.baseproject.R;
+import com.ruiqin.baseproject.base.BaseFragment;
+
+public class BlankFragment extends BaseFragment {
+    @Override
+    protected void initView(View view, Bundle savedInstanceState) {
+        
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_blank;
+    }
+}
