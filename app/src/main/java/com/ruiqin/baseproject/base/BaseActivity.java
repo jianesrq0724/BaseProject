5c43948aa5bbee31f645e852a93c4ac85bb00ead
shenruiqin <shenruiqin@100cb.cn>
2017-07-14 10:13:09 +0800

    增加rxjava2

--- app/src/main/java/com/ruiqin/baseproject/base/BaseActivity.java
+++ app/src/main/java/com/ruiqin/baseproject/base/BaseActivity.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-
 import com.ruiqin.baseproject.R;
 import com.ruiqin.baseproject.util.InstanceUtil;
 
