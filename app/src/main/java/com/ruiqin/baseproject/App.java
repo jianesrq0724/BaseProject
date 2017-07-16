a4a5e569ba607260247587a7af06e5febcd866a5
ruiqin.shen <jianesrq0724@outlook.com>
2017-07-16 12:45:35 +0800

    集成X5WebView

--- app/src/main/java/com/ruiqin/baseproject/App.java
+++ app/src/main/java/com/ruiqin/baseproject/App.java
@@ -7,6 +7,8 @@ import android.support.multidex.MultiDex;
 import com.blankj.utilcode.util.Utils;
 import com.ruiqin.baseproject.greendao.gen.DaoMaster;
 import com.ruiqin.baseproject.greendao.gen.DaoSession;
+import com.ruiqin.baseproject.util.LogUtils;
+import com.tencent.smtt.sdk.QbSdk;
 
 import org.greenrobot.greendao.database.Database;
 
@@ -39,7 +41,30 @@ public class App extends Application {
         DaoMaster.DevOpenHelper devOpenHelper = new DaoMaster.DevOpenHelper(mContext, "test-db");
         Database writableDb = devOpenHelper.getWritableDb();
         daoSession = new DaoMaster(writableDb).newSession();
+
+        initX5Environment();//x5内核初始化接口
     }
+
+    /**
+     * x5内核初始化接口
+     */
+    private void initX5Environment() {
+        //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
+        QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
+            @Override
+            public void onViewInitFinished(boolean arg0) {
+                //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
+                LogUtils.e("app", " onViewInitFinished is " + arg0);
+            }
+
+            @Override
+            public void onCoreInitFinished() {
+            }
+        };
+        //x5内核初始化接口
+        QbSdk.initX5Environment(getApplicationContext(), cb);
+    }
+
 
     public static final Context getContext() {
         return mContext;
